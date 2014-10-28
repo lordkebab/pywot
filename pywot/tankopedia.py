a9b4139be337f5ce66a4497bbbcffca4a83bb6d1
Matt Selph <matt.selph@gmail.com>
2014-10-28 13:21:07 -0400

    Vehicle details method complete

--- pywot/tankopedia.py
+++ pywot/tankopedia.py
@@ -10,6 +10,16 @@ class Tankopedia(API):
 		if type(fields) is list:
 			fields = self._format_fields(fields)
 
-		print self._api_call(endpoint=endpoint, fields=fields, language=language)
+		return self._api_call(endpoint=endpoint, fields=fields, language=language)
+
+	def vehicle_details(self, tank_id=None, fields='', language='en'):
+		endpoint = '/encyclopedia/tankinfo/'
+
+		if type(fields) is list:
+			fields = self._format_fields(fields)
+
+		return self._api_call(endpoint=endpoint, tank_id=tank_id, fields=fields, language=language)
+
+
 
 
