79b2513fe792890ed26b9e02727f36293e2eab15
Matt Selph <matt.selph@gmail.com>
2014-10-28 13:41:55 -0400

    Completed engines and turrets methods

--- pywot/tankopedia.py
+++ pywot/tankopedia.py
@@ -20,6 +20,24 @@ class Tankopedia(API):
 
 		return self._api_call(endpoint=endpoint, tank_id=tank_id, fields=fields, language=language)
 
+	def engines(self, fields='', language='en', module_id='', nation=''):
+		endpoint = '/encyclopedia/tankengines/'
+
+		if type(fields) is list:
+			fields = self._format_fields(fields)
+
+		return self._api_call(endpoint=endpoint, fields=fields, language=language)
+
+	def turrets(self, fields='', language='en', module_id='', nation=''):
+		endpoint = '/encyclopedia/tankturrets/'
+
+		if type(fields) is list:
+			fields = self._format_fields(fields)
+
+		return self._api_call(endpoint=endpoint, fields=fields, language=language)
+
+	
+
 
 
 
