160d37f971d20d0e7372254c0a330a7724b00248
Matt Selph <matt.selph@gmail.com>
2014-10-28 13:07:47 -0400

    Added method to format fields as comma-delimited string

--- pywot/api.py
+++ pywot/api.py
@@ -10,15 +10,17 @@ class API(object):
 	def __init__(self, app_id):
 		self.app_id = app_id
 
-	def _api_call(self, url, **kwargs):
+	def _format_fields(self, fields):
+		return ','.join(fields)
+
+	def _api_call(self, endpoint, **kwargs):
 		''' Initiates an API call '''
 		
 		payload = kwargs
-		
+
 		# add the application id to our payload
 		payload['application_id'] = self.app_id
-		r = requests.get(url, params=payload)
+		r = requests.get(self.BASE_URL+endpoint, params=payload)
 		
-		# return pretty-printed json
 		return json.dumps(r.json(), sort_keys=True, indent=4)
 				
