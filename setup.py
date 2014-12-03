d0c96b14a0f1f8464bac9fbe85f8c76f3285b05b
Matt Selph <matt.selph@gmail.com>
2014-12-03 10:07:08 -0500

    Added requests python module dependency

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 setup(
 	name='pywot', 
-	packages=['requests','pywot'], 
+	packages=['pywot'], 
 	version='0.1', 
 	description='A python interface to Wargaming.net\'s World of Tanks API', 
+	install_requires=[
+		'requests'
+	]
 	author='Matt Selph', 
 	author_email='matt.selph@gmail.com', 
 	url='https://github.com/mattselph/pywot', 
