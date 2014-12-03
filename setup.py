215a20deffffcd7a0ef4045c1c517fc5cf6ad4b6
Matt Selph <matt.selph@gmail.com>
2014-12-03 09:53:53 -0500

    Added pypi setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='pywot', 
+	packages=['requests','pywot'], 
+	version='0.1', 
+	description='A python interface to Wargaming.net\'s World of Tanks API', 
+	author='Matt Selph', 
+	author_email='matt.selph@gmail.com', 
+	url='https://github.com/mattselph/pywot', 
+	download_url='https://github.com/mattselph/pywot/tarball/0.1',
+	keywords=['world of tanks', 'wargaming', 'wot'],
+	classifiers=[]
+)
